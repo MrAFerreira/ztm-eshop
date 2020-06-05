ed17c4a6693b59df24b18a7dc902288321cfd0c8
Andre Ferreira <mrawesomeferreira@gmail.com>
2020-06-05 14:56:35 +0100

    added simple backend server for stripe payments

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = 'pk_test_hOrvmmd5fhE9kFdFg0LO7nnN00Sz3gpkNH';
 
   const onToken = (token) => {
-    console.log(token);
-    alert('Payment successfull');
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token,
+      },
+    })
+      .then((response) => {
+        alert('Payment successful');
+      })
+      .catch((error) => {
+        console.log('Payment error: ', JSON.parse(error));
+        alert(
+          'There was an issue with your payment. Please make sure you use the provided credit card.'
+        );
+      });
   };
 
   return (
