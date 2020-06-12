b22a631d2f4ea090be860ba23e6f7e07f4747159
Andre Ferreira <mrawesomeferreira@gmail.com>
2020-06-12 11:18:34 +0100

    started implementing lazy loading in some features

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,21 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, lazy, Suspense } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
 import { GlobalStyle } from './global.styles';
 
-import HomePage from './views/homepage/homepage.component';
-import ShopPage from './views/shop/shop.component';
-import Authentication from './views/authentication/authentication.component';
-import Checkout from './views/checkout/checkout.component';
+import Spinner from './components/spinner/spinner.component';
 import Header from './components/header/header.component';
+import ErrorBoundary from './components/error-boundary/error-boundary.component';
 
 import { selectCurrentUser } from './redux/user/user.selectors';
 import { checkUserSession } from './redux/user/user.actions';
+
+const HomePage = lazy(() => import('./views/homepage/homepage.component'));
+const ShopPage = lazy(() => import('./views/shop/shop.component'));
+const Authentication = lazy(() => import('./views/authentication/authentication.component'));
+const Checkout = lazy(() => import('./views/checkout/checkout.component'));
 
 const App = ({ checkUserSession, currentUser }) => {
   useEffect(() => {
@@ -24,14 +27,19 @@ const App = ({ checkUserSession, currentUser }) => {
       <GlobalStyle />
       <Header />
       <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/shop" component={ShopPage} />
-        <Route exact path="/checkout" component={Checkout} />
-        <Route
-          exact
-          path="/authentication"
-          render={() => (currentUser ? <Redirect to="/" /> : <Authentication />)}
-        />
+        <ErrorBoundary>
+          <Suspense fallback={<Spinner />}>
+            <Route exact path="/" component={HomePage} />
+            333333
+            <Route path="/shop" component={ShopPage} />
+            <Route exact path="/checkout" component={Checkout} />
+            <Route
+              exact
+              path="/authentication"
+              render={() => (currentUser ? <Redirect to="/" /> : <Authentication />)}
+            />
+          </Suspense>
+        </ErrorBoundary>
       </Switch>
     </div>
   );
